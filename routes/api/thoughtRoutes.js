e51823425fcdd9a3f41eecd9162e745fdbac15c7
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-09-01 03:20:06 -0600

    added thoughtRoutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getOneThought,
+    createOneThought,
+    updateOneThought,
+    deleteThought,
+    addOneReaction,
+    removeOneReaction,
+} = require('../../controllers/thought-controller');
+
+
+// /api/thoughts routes
+router.route('/').get(getThoughts).post(createOneThought);
+
+// /api/thoughts/:thoughtId routes
+router.route('/').get(getOneThought).post(updateOneThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions 
+router.route('/:thoughtId/reactions').post(addOneReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeOneReaction);
+
+module.exports = router;
