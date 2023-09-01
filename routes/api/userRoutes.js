e156bd92c1dfc0f93d164997fa329f2f89a71c2e
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-09-01 03:56:24 -0600

    added user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getOneUsers,
+    createOneUser,
+    updateOneUser,
+    deleteOneUser,
+    addNewFriend,
+    removeOneFriend,
+} = require('../../controllers/user-controller');
+
+// /api/users
+router.route('/').get(getAllUsers).post(getOneUsers);
+
+// /api/users/:userId
+router.route('/:userId').get(createOneUser).put(updateOneUser).delete(deleteOneUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addNewFriend).delete(removeOneFriend);
+
+module.exports = router;
